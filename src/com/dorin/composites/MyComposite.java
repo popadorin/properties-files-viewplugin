896bb91044e81b1e3237454df89c4d06b5e1bbf9
Dorin <dorin.popa1995@gmail.com>
2017-08-24 18:21:39 +0300

    Create openFile method and a filter for .properties files

--- src/com/dorin/composites/MyComposite.java
+++ src/com/dorin/composites/MyComposite.java
@@ -1,12 +1,22 @@
 package com.dorin.composites;
 
-import org.eclipse.swt.widgets.Composite;
-
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 public class MyComposite extends Composite {
 	private Table table;
@@ -20,6 +30,12 @@ public class MyComposite extends Composite {
 		super(parent, style);
 		
 		Button btnNewButton = new Button(this, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				loadFiles();
+			}
+		});
 		btnNewButton.setBounds(10, 25, 75, 25);
 		btnNewButton.setText("New file");
 		
@@ -50,10 +66,41 @@ public class MyComposite extends Composite {
 	    
 	}
 
-	@Override
-	protected void checkSubclass() {
-		// Disable the check that prevents subclassing of SWT components
+	private void loadFiles() {
+		try {
+			// Iterate through files and get the ones which have extension .properties
+			ResourcesPlugin.getWorkspace().getRoot().accept(new IResourceVisitor() {
+				
+				@Override
+				public boolean visit(IResource resource) throws CoreException {
+					if (resource.getName().endsWith(".properties")) {
+						System.out.println("resource name: " + resource.getName());
+						System.out.println("resource project: " + resource.getProject());
+						IFile file = (IFile) resource;
+						openFile(file);
+					}
+					
+					return true;
+				}
+				
+			}, 2, false);
+			
+		} catch (Exception e) {
+			System.out.println("Exception bad");
+			e.printStackTrace();
+		}
+		System.out.println("finished checking files");
 	}
 	
+	private void openFile(IFile file) {
+	    IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	    try {
+			IDE.openEditor(page, file);
+		} catch (PartInitException e) {
+			System.out.println("The partInitException occured");
+			e.printStackTrace();
+		}
+		    
+	}
 	
 }
